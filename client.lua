58a24cab409f75bb88d59c3d9a1f6138e8cb4598
szlifi <100534265+Szlifi@users.noreply.github.com>
2022-08-16 11:02:42 +0200

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,5 @@
+AddEventHandler("playerSpawnd", function(spawn)
+    TriggerEvent('chat:addMessage', { 
+        template = '<div style="padding: 0.5vw; margin: 0.5vw; background-color: rgba(41, 41, 41, 0.6); border-radius: 15px;"><i class="fa fa-cog" aria-hidden="true"></i>^1^*[Szerver Üzenet] ^0^*Sikeresen felcsatlakoztál a szervere, jó játékot!</div>',
+    })
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+
+games { 'gta5' }
+
+author 'szlifi'
+
+description 'spawn chat message'
+
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
